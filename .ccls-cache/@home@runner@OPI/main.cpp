da45ee917f03489df8c0ed3dd7cad793af8073d8
f1q1q1 <78561224+DimaShlyahtitskiy@users.noreply.github.com>
2023-03-23 13:39:45 +0000

    array commit1

--- .ccls-cache/@home@runner@OPI/main.cpp
+++ .ccls-cache/@home@runner@OPI/main.cpp
@@ -1,5 +1,38 @@
 #include <iostream>
 
+using namespace std;
+
+void func(int arr[],int n) //заголовок функции
+{
+  int sum=0;
+  int fact=1;
+  for(int i =0;i<n;i++)//нахождение суммы всех не парных элементов с знаком +
+    {
+      if(arr[i]>0&&(i+1)%2!=0)
+      {
+        sum+=arr[i];
+      }
+    }
+  for (int i =0;i<n;i++)//замена всех парных элементов
+    {
+      if((i+1)%2==0)
+      {
+        arr[i]=sum;
+      }
+    }
+}
+
 int main() {
-  std::cout << "Hello World!\n";
+  const int n=15;
+  int arr[n];
+  for(int i = 0; i< n; i++)//инициализация массива
+    {
+      cin >>arr[i];
+    }
+  func(arr,n);//вызов функции
+  for(int i=0;i<n;i++)//вывод масива
+    {
+      cout << arr[i]<<"\t";
+    }
+    return 0;
 }
