1d9d0a1d0e150b9b3a51f2b91b32b164dc5070cb
f1q1q1 <78561224+DimaShlyahtitskiy@users.noreply.github.com>
2023-03-23 13:46:49 +0000

    array commit branch 2

--- .ccls-cache/@home@runner@OPI/main.cpp
+++ .ccls-cache/@home@runner@OPI/main.cpp
@@ -2,7 +2,32 @@
 
 using namespace std;
 
-void func(int arr[],int n) //заголовок функции
+int func(int arr[],int n) //заголовок функции
 {
-  cout << "hi";
+  int fact=1;
+  for(int i =0;i<n;i++)
+    {
+      if(arr[i]<8)//проверка на то что число не больше 8
+      {
+        for( ;arr[i]>1;arr[i]--)//вычисление факториала
+          {
+            fact*=arr[i];
+          }
+        break;//вызод из цикла так как нас интересует только первое число что истинно условию
+      }
+    }
+  return fact;//возвращаем значение
+}
+
+int main() {
+  int fact;
+  const int n=15;
+  int arr[n];
+  for(int i = 0; i< n; i++)//инициализация массива
+    {
+      cin >>arr[i];
+    }
+  fact=func(arr,n);//вызов функции
+  cout << fact;
+    return 0;
 }
